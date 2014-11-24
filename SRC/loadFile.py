ed5ffb87d529cdbed9ce10c2922f26f4443219eb
niteshsinghania <niteshsinghania@hotmail.com>
2014-11-23 23:34:11 -0600

    Added src

---
+++ SRC/loadFile.py
@@ -0,0 +1,21 @@
+import numpy as np   # gets some useful functions such as special arrays, dot-product, etc.
+import pymysql
+
+
+def load_data_from_file():
+    
+    matrix = []
+    
+    conn = pymysql.connect(host='attiyat.net', port=3306, user='dm', passwd='encrypted', db='baseball')
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT a.G,a.W,a.L,a.R,a.AB,a.H,a.2B,a.3B,a.HR,a.BB,a.SO,a.SB,a.RA,a.ER,a.ERA,a.HA,a.HRA,a.BBA,a.SOA,a.E,b.G,b.RBI FROM Teams a INNER JOIN Batting b ON a.teamID = b.teamID and a.yearID = b.yearID")
+
+
+    for row in cur:
+        matrix.append(row)
+
+    cur.close()
+    conn.close()
+    return matrix
