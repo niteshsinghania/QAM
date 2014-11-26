5ec10681e0d434e6584c96f130e72afea96ae025
niteshsinghania <niteshsinghania@hotmail.com>
2014-11-25 22:27:34 -0600

    Fixed bugs

--- SRC/myAssociationMining.py
+++ SRC/myAssociationMining.py
@@ -13,7 +13,6 @@ filter = 0
 # Read the data (Q1)
 dataset = km.load_dataset()
 
-print dataset
 
 # When you get to part 7. you might wish to come back and
 # filter the data here - you could optionally have a variable
